11d44bae77afbe2f382fb540f0d1439625b87673
Anantharajts <anantharajtsananthu@gmail.com>
2026-02-14 13:03:58 +0530

    added

---
+++ admin.php
@@ -0,0 +1,13 @@
+<?php
+include('database.php');
+include('admin_header.php');
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col bg-info">a</div>
+        <div class="col bg-info">a</div>
+        <div class="col bg-info">a</div>
+        <div class="col bg-info">a</div>
+    </div>
+</div>
